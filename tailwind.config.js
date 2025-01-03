07f9df122a0c24d681c0204851bd257afe6653f3
ye1n <uyeinyein@gmail.com>
2025-01-03 14:29:29 +0900

    feat: tailwind.config.js custom colors 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,36 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        custom: {
+          black: "#222222",
+          white: "#ffffff",
+          primary: "#0f54df",
+          secondary: "#ff9015",
+          grey: {
+            1: "#444444",
+            2: "#888888",
+            4: "#bdbdbd",
+          },
+          form: {
+            line: "#e0e0e0",
+            bg: "#eeeeee",
+            acc: "#fffaf0",
+          },
+          label: {
+            line: "#c1d6e9",
+            bg: "#f0f4f9",
+          },
+          bg: "#e4ecff",
+          btn: {
+            default: "#244072",
+            inForm: "#d8d8d8",
+            line: "#f2f2f2",
+          },
+        },
+      },
+    },
   },
   plugins: [],
 };
