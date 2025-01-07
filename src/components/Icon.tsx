1bb8d65f55d6b47b435c9c93f381c1fd85201281
ye1n <uyeinyein@gmail.com>
2025-01-07 14:58:57 +0900

    feat: Icon component 추가

---
+++ src/components/Icon.tsx
@@ -0,0 +1,40 @@
+interface Props {
+  icon: any;
+  width?: string;
+  height?: string;
+  className?: string;
+}
+
+const Icon: React.FC<Props> = ({
+  icon,
+  width = "16",
+  height = "16",
+  className = "",
+  ...props
+}) => {
+  // svg
+  if (typeof icon === "function") {
+    const SVGIcon = icon;
+    return (
+      <SVGIcon width={width} height={height} className={className} {...props} />
+    );
+  }
+
+  // png
+  if (typeof icon === "string") {
+    return (
+      <img
+        src={icon}
+        width={width}
+        height={height}
+        alt=""
+        className={className}
+        {...props}
+      />
+    );
+  }
+
+  return null;
+};
+
+export default Icon;
