ed1a183f8f93860995cff4b56f1f810ee3cddea1
ye1n <uyeinyein@gmail.com>
2025-01-07 15:00:54 +0900

    refactor: Layout 가로 x 세로 추가

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -2,9 +2,8 @@ import { Outlet } from "react-router-dom";
 
 export default function Layout() {
   return (
-    // 전체 레이아웃에서 세로 가로 스크롤 없어야함, 부분적 스크롤은 필요
-    // <div className="w-[1920px] h-screen border-2">
-    <Outlet />
-    // </div>
+    <div className="w-[1920px] h-[1080px]">
+      <Outlet />
+    </div>
   );
 }
