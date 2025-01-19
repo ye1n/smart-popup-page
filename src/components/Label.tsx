73ba3df0960f9b8ae85c5bf5262292d3c5c338e1
ye1n <uyeinyein@gmail.com>
2025-01-19 21:54:17 +0900

    feat: Label component className props 추가

--- src/components/Label.tsx
+++ src/components/Label.tsx
@@ -1,10 +1,11 @@
 type Props = {
   label: string;
+  className?: string;
 };
 
-const Label: React.FC<Props> = ({ label }) => {
+const Label: React.FC<Props> = ({ label, className = "" }) => {
   return (
-    <div className="flex items-center gap-1.5">
+    <div className={`flex items-center gap-1.5 ${className}`}>
       <div className="w-0.5 h-[9px] bg-custom-black" />
       <p className="pt-1 text-xs font-bold text-custom-primary">{label}</p>
     </div>
