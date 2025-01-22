839dba737301da24b71295b651f8161bc8421194
ye1n <uyeinyein@gmail.com>
2025-01-22 18:07:09 +0900

    refactor: MainPage 페이지별 버튼 분류

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Layout from "./layouts/Layout";
-import Page1 from "./pages/p1/Page1";
-import Page2 from "./pages/p2/Page2";
+import Page1 from "./pages/s/p1/Page1";
+import Page2 from "./pages/s/p2/Page2";
 import MainPage from "./pages/MainPage";
 
 function App() {
@@ -11,10 +11,19 @@ function App() {
       <Route element={<Layout />}>
         {/* 메인 */}
         <Route path="/" element={<MainPage />} />
+
         {/* 심사원장 */}
-        <Route path="/p1" element={<Page1 />} />
+        <Route path="/s-p1" element={<Page1 />} />
+        <Route path="/s-p2" element={<Page2 />} />
+        <Route path="/s-p3" element={<></>} />
+        <Route path="/s-p4" element={<></>} />
+        <Route path="/s-p5" element={<></>} />
+        <Route path="/s-p6" element={<></>} />
+
         {/* 채권원장 */}
-        <Route path="/p2" element={<Page2 />} />
+        <Route path="/c-p1" element={<></>} />
+        <Route path="/c-p2" element={<></>} />
+        <Route path="/c-p3" element={<></>} />
       </Route>
     </Routes>
   );
--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,12 +1,7 @@
 import { useState } from "react";
 import Button from "../components/Button";
 import Dropdown from "../components/Dropdown";
-
-type PopupOptions = {
-  route: string;
-  width: number;
-  height: number;
-};
+import Label from "../components/Label";
 
 export default function MainPage() {
   const [windowSize, setWindowSize] = useState<number>(0);
@@ -18,9 +13,9 @@ export default function MainPage() {
     { width: 1920, height: 1080 },
   ];
 
-  const openPopup = (options: PopupOptions) => {
-    const { route, width, height } = options;
-
+  const openPopup = (route: string) => {
+    const width = windowSizeList[windowSize].width;
+    const height = windowSizeList[windowSize].height;
     const left = window.screenX + (window.outerWidth - width) / 2;
     const top = window.screenY + (window.outerHeight - height) / 2;
     const windowFeatures = `width=${width},height=${height},left=${left},top=${top},scrollbars=no,resizable=no`;
@@ -28,44 +23,86 @@ export default function MainPage() {
   };
 
   return (
-    <div>
-      <Dropdown
-        width="w-[100px]"
-        className="border"
-        options={[
-          {
-            label: "1280 X 720",
-            value: 0,
-          },
-          {
-            label: "1366 X 768",
-            value: 1,
-          },
-          {
-            label: "1600 X 900",
-            value: 2,
-          },
-          {
-            label: "1920 X 1080",
-            value: 3,
-          },
-        ]}
-        onChange={(value) => {
-          setWindowSize(Number(value));
-        }}
-      />
-      <p>심사원장</p>
-      <Button
-        label="심사원장"
-        onClick={() =>
-          openPopup({
-            route: "/p1",
-            width: windowSizeList[windowSize].width,
-            height: windowSizeList[windowSize].height,
-          })
-        }
-      />
-      <p>채권원장</p>
+    <div className="flex flex-col gap-6 p-4">
+      <div className="flex items-center gap-2">
+        <p className="text-xs font-medium">해상도</p>
+        <Dropdown
+          align="center"
+          className="border"
+          options={[
+            {
+              label: "1280 X 720",
+              value: 0,
+            },
+            {
+              label: "1366 X 768",
+              value: 1,
+            },
+            {
+              label: "1600 X 900",
+              value: 2,
+            },
+            {
+              label: "1920 X 1080",
+              value: 3,
+            },
+          ]}
+          onChange={(value) => {
+            setWindowSize(Number(value));
+          }}
+        />
+      </div>
+      <div className="flex flex-col gap-1">
+        <Label label="심사원장" />
+        <Button
+          className="w-fit"
+          label="smartsb_심사원장_1920_기본정보+개인사업자"
+          onClick={() => openPopup("/s-p1")}
+        />
+        <Button
+          className="w-fit"
+          label="smartsb_심사원장_1920_자산정보+직장인"
+          onClick={() => openPopup("/s-p2")}
+        />
+        <Button
+          className="w-fit"
+          label="smartsb_심사원장_1920_DSR산출정보+기타"
+          onClick={() => openPopup("/s-p3")}
+        />
+        <Button
+          className="w-fit"
+          label="smartsb_심사원장_1920_스코어+햇살론"
+          onClick={() => openPopup("/s-p4")}
+        />
+        <Button
+          className="w-fit"
+          label="smartsb_심사원장_1920_송금정보"
+          onClick={() => openPopup("/s-p5")}
+        />
+        <Button
+          className="w-fit"
+          label="smartsb_콜원장"
+          onClick={() => openPopup("/s-p6")}
+        />
+      </div>
+      <div className="flex flex-col gap-1">
+        <Label label="채권원장" />
+        <Button
+          className="w-fit"
+          label="smartsb_채권원장_1920"
+          onClick={() => openPopup("/c-p1")}
+        />
+        <Button
+          className="w-fit"
+          label="smartsb_채권원장_1240_온라인조회_Atype"
+          onClick={() => openPopup("/c-p2")}
+        />
+        <Button
+          className="w-fit"
+          label="smartsb_채권원장_1240_사고관리"
+          onClick={() => openPopup("/c-p3")}
+        />
+      </div>
     </div>
   );
 }
