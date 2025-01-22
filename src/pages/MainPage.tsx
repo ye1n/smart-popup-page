f7fe7f80960cb12f0e083f3b03510e0fbd40049b
ye1n <uyeinyein@gmail.com>
2025-01-22 11:51:25 +0900

    feat: MainPage window size 조절 dropdown 추가

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,4 +1,6 @@
+import { useState } from "react";
 import Button from "../components/Button";
+import Dropdown from "../components/Dropdown";
 
 type PopupOptions = {
   route: string;
@@ -7,21 +9,61 @@ type PopupOptions = {
 };
 
 export default function MainPage() {
+  const [windowSize, setWindowSize] = useState<number>(0);
+
+  const windowSizeList = [
+    { width: 1280, height: 720 },
+    { width: 1366, height: 768 },
+    { width: 1600, height: 900 },
+    { width: 1920, height: 1080 },
+  ];
+
   const openPopup = (options: PopupOptions) => {
     const { route, width, height } = options;
 
     const left = window.screenX + (window.outerWidth - width) / 2;
     const top = window.screenY + (window.outerHeight - height) / 2;
-    const windowFeatures = `width=${width},height=${height},left=${left},top=${top},toolbar=no,menubar=no,location=no,status=no,scrollbars=no,resizable=no`;
+    const windowFeatures = `width=${width},height=${height},left=${left},top=${top},scrollbars=no,resizable=no`;
     window.open(route, "_blank", windowFeatures);
   };
 
   return (
     <div>
+      <Dropdown
+        width="w-[100px]"
+        className="border"
+        options={[
+          {
+            label: "1280 X 720",
+            value: 0,
+          },
+          {
+            label: "1366 X 768",
+            value: 1,
+          },
+          {
+            label: "1600 X 900",
+            value: 2,
+          },
+          {
+            label: "1920 X 1080",
+            value: 3,
+          },
+        ]}
+        onChange={(value) => {
+          setWindowSize(Number(value));
+        }}
+      />
       <p>심사원장</p>
       <Button
         label="심사원장"
-        onClick={() => openPopup({ route: "/p1", width: 1920, height: 1080 })}
+        onClick={() =>
+          openPopup({
+            route: "/p1",
+            width: windowSizeList[windowSize].width,
+            height: windowSizeList[windowSize].height,
+          })
+        }
       />
       <p>채권원장</p>
     </div>
