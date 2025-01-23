ef7b2e9d4bb35c4e6ecff5f0d1db4deab434d956
ye1n <uyeinyein@gmail.com>
2025-01-23 21:06:45 +0900

    refactor: Table width, borderTop props 추가

--- src/components/table/Table.tsx
+++ src/components/table/Table.tsx
@@ -2,35 +2,37 @@ import React from "react";
 
 type Props = {
   minWidth?: number;
+  width?: number;
   height?: number;
   className?: string;
-  colWidths?: string[];
+  borderTop?: boolean;
   children?: React.ReactNode;
 };
 
 const Table: React.FC<Props> = ({
   minWidth,
+  width,
   height,
   className = "",
-  colWidths,
+  borderTop = true,
   children,
 }) => {
   return (
     <div
-      className={`w-full border-t border-t-[#333333] overflow-auto ${className}`}
-      style={{ height: height ? `${height}px` : "auto" }}
+      className={`overflow-auto ${className} ${
+        borderTop ? "border-t border-t-[#333333]" : ""
+      }`}
+      style={{
+        width: width ? `${width}px` : "100%",
+        height: height ? `${height}px` : "auto",
+      }}
     >
       <table
-        className="w-full"
+        className={`w-full ${
+          !borderTop ? "border-t border-custom-form-line" : ""
+        }`}
         style={{ minWidth: minWidth ? `${minWidth}px` : "auto" }}
       >
-        {colWidths && (
-          <colgroup>
-            {colWidths.map((width, index) => (
-              <col key={index} style={{ width }} />
-            ))}
-          </colgroup>
-        )}
         {children && children}
       </table>
     </div>
