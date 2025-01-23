6b750847731a3944194ff9218cd03a63d76737c8
ye1n <uyeinyein@gmail.com>
2025-01-23 17:28:52 +0900

    feat: Button variant icon-white-outline 추가

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,9 +3,10 @@ type Props = {
     | "blue"
     | "white-outline-shadow"
     | "white-outline"
+    | "icon-white-outline"
     | "gray"
     | "table-gray";
-  label: string;
+  label?: string;
   icon?: React.ReactNode;
   iconPosition?: "left" | "right";
   className?: string;
@@ -26,6 +27,8 @@ const Button: React.FC<Props> = ({
       "border border-custom-btn-line text-custom-gray-1 bg-custom-white shadow font-medium h-[24px] rounded px-2",
     "white-outline":
       "border border-custom-gray-4 text-custom-gray-1 bg-custom-white font-medium h-[24px] rounded px-2",
+    "icon-white-outline":
+      "border border-custom-gray-4 bg-custom-white w-[24px] h-[24px] flex items-center justify-center rounded",
     gray: "bg-custom-gray-3 text-custom-white font-bold h-[24px] rounded px-2",
     "table-gray":
       "bg-custom-btn-inForm h-[18px] text-custom-gray-1 text-[10px] px-1.5",
@@ -37,7 +40,7 @@ const Button: React.FC<Props> = ({
       onClick={onClick}
     >
       {icon && iconPosition === "left" && icon}
-      <p className="text-xs">{label}</p>
+      {label && <p className="text-xs">{label}</p>}
       {icon && iconPosition === "right" && icon}
     </button>
   );
