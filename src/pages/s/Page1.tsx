65ac34b15bd3e484a0370132128b8abc0bc28a11
ye1n <uyeinyein@gmail.com>
2025-01-23 17:31:10 +0900

    feat: 심사원장 > 고객정보 > 송금정보 table 추가

--- src/pages/s/Page1.tsx
+++ src/pages/s/Page1.tsx
@@ -11,7 +11,6 @@ import IncomeInfoTable from "./components/IncomeInfoTable";
 import Tbody from "../../components/table/Tbody";
 import Tr from "../../components/table/Tr";
 import Td from "../../components/table/Td";
-import Table from "../../components/table/Table";
 import AgreeInfoTable from "./components/AgreeInfoTable";
 import SideMenu from "../../components/common/SideMenu";
 import { useState } from "react";
@@ -20,6 +19,9 @@ import AssetsInfoTable from "./components/customInfo/AssetsInfoTable";
 import DsrInfoTable02 from "./components/customInfo/DsrInfoTable02";
 import DsrInfoTable01 from "./components/customInfo/DsrInfoTable01";
 import ScoreTable from "./components/customInfo/ScoreTable";
+import TransferInfoTable01 from "./components/customInfo/TransferInfoTable01";
+import TransferInfoTable02 from "./components/customInfo/TransferInfoTable02";
+import Table from "../../components/table/Table";
 
 export default function Page1() {
   const customInfoTabs = [
@@ -282,7 +284,227 @@ export default function Page1() {
                   <ScoreTable />
                 </div>
               ) : (
-                <></>
+                <div className="flex flex-col h-[298px] overflow-y-auto">
+                  <div className="mb-2">
+                    <div className="flex items-end justify-between mb-2">
+                      <p className="text-xs font-bold text-custom-black">
+                        CMS내역 (A)
+                      </p>
+                      <div className="flex gap-1">
+                        <Button
+                          variant="icon-white-outline"
+                          label=""
+                          icon={<Icon icon={icons.roundPlus} />}
+                          onClick={() => {}}
+                        />
+                        <Button
+                          variant="icon-white-outline"
+                          label=""
+                          icon={<Icon icon={icons.view} />}
+                          onClick={() => {}}
+                        />
+                        <Button
+                          variant="icon-white-outline"
+                          label=""
+                          icon={<Icon icon={icons.lock} />}
+                          onClick={() => {}}
+                        />
+                        <Button
+                          variant="icon-white-outline"
+                          label=""
+                          icon={<Icon icon={icons.unlock} />}
+                          onClick={() => {}}
+                        />
+                        <Button
+                          variant="icon-white-outline"
+                          label=""
+                          icon={<Icon icon={icons.edit} />}
+                          onClick={() => {}}
+                        />
+                        <Button
+                          variant="icon-white-outline"
+                          label=""
+                          icon={<Icon icon={icons.remove} />}
+                          onClick={() => {}}
+                        />
+                      </div>
+                    </div>
+                    <TransferInfoTable01 />
+                  </div>
+                  <div className="mb-[26px]">
+                    <div className="flex items-end justify-between mb-2">
+                      <p className="text-xs font-bold text-custom-black">
+                        계좌내역 (B)
+                      </p>
+                      <div className="flex gap-1">
+                        <Button
+                          variant="white-outline-shadow"
+                          label="당행자체대환"
+                          icon={<Icon icon={icons.chk} />}
+                          onClick={() => {}}
+                        />
+                        <Button
+                          variant="white-outline-shadow"
+                          label="권리침해대환"
+                          icon={<Icon icon={icons.chk} />}
+                          onClick={() => {}}
+                        />
+                        <Button
+                          variant="white-outline-shadow"
+                          label="대환"
+                          icon={<Icon icon={icons.chk} />}
+                          onClick={() => {}}
+                        />
+                        <Button
+                          variant="icon-white-outline"
+                          label=""
+                          icon={<Icon icon={icons.roundPlus} />}
+                          onClick={() => {}}
+                        />
+                        <Button
+                          variant="icon-white-outline"
+                          label=""
+                          icon={<Icon icon={icons.view} />}
+                          onClick={() => {}}
+                        />
+                        <Button
+                          variant="icon-white-outline"
+                          label=""
+                          icon={<Icon icon={icons.lock} />}
+                          onClick={() => {}}
+                        />
+                        <Button
+                          variant="icon-white-outline"
+                          label=""
+                          icon={<Icon icon={icons.unlock} />}
+                          onClick={() => {}}
+                        />
+                        <Button
+                          variant="icon-white-outline"
+                          label=""
+                          icon={<Icon icon={icons.edit} />}
+                          onClick={() => {}}
+                        />
+                        <Button
+                          variant="icon-white-outline"
+                          label=""
+                          icon={<Icon icon={icons.remove} />}
+                          onClick={() => {}}
+                        />
+                      </div>
+                    </div>
+                    <TransferInfoTable02 />
+                  </div>
+                  <table className="mb-4 border-t border-custom-form-line">
+                    <Tbody>
+                      <Tr>
+                        <Td type="label" align="left">
+                          보증보험 보증료(분할납부예상액)
+                        </Td>
+                        <Td>
+                          <Input
+                            align="right"
+                            defaultValue="5,730"
+                            isRequired
+                            readOnly
+                          />
+                        </Td>
+                        <Td type="label" align="left">
+                          보증실행 전문 결과 보증료
+                        </Td>
+                        <Td>
+                          <Input
+                            align="right"
+                            defaultValue="5,730"
+                            isRequired
+                            readOnly
+                          />
+                        </Td>
+                      </Tr>
+                    </Tbody>
+                  </table>
+                  <div className="mb-4">
+                    <div className="mb-2">
+                      <p className="text-xs font-bold text-custom-black">
+                        인지세 설정 (C)
+                      </p>
+                    </div>
+                    <Table>
+                      <Tbody>
+                        <Tr>
+                          <Td type="label" align="left">
+                            인지세
+                          </Td>
+                          <Td type="text" align="right">
+                            100,000,000
+                          </Td>
+                          <Td type="label" align="left">
+                            총 이체금액(A+B+C)
+                          </Td>
+                          <Td type="text" align="right">
+                            100,000,000
+                          </Td>
+                          <Td type="label" align="left">
+                            대출액
+                          </Td>
+                          <Td type="text" align="right">
+                            100,000,000
+                          </Td>
+                          <Td type="label" align="left">
+                            결과
+                          </Td>
+                          <Td type="text" align="left">
+                            완료
+                          </Td>
+                        </Tr>
+                      </Tbody>
+                    </Table>
+                  </div>
+                  <div>
+                    <div className="flex items-end justify-between mb-2">
+                      <p className="text-xs font-bold text-custom-black">
+                        기타정보
+                      </p>
+                      <Button
+                        variant="white-outline-shadow"
+                        label="NCCS자서대행"
+                        onClick={() => {}}
+                      />
+                    </div>
+                    <Table>
+                      <Tbody>
+                        <Tr>
+                          <Td type="label" align="left">
+                            공증/인감
+                          </Td>
+                          <Td type="text" colSpan={3}></Td>
+                        </Tr>
+                        <Tr>
+                          <Td type="label" align="left">
+                            수령지주소
+                          </Td>
+                          <Td type="text" align="left" colSpan={3}>
+                            경기도 성남시 중원구 갈마치로 288 14 A-730
+                          </Td>
+                        </Tr>
+                        <Tr>
+                          <Td type="label" align="left">
+                            CMS은행
+                          </Td>
+                          <Td type="text" align="left">
+                            스마트저축은행
+                          </Td>
+                          <Td type="label" align="left">
+                            CMS계좌
+                          </Td>
+                          <Td type="text" align="left">
+                            123456-789-00-45613
+                          </Td>
+                        </Tr>
+                      </Tbody>
+                    </Table>
+                  </div>
+                </div>
               )}
             </div>
             {/* 소득정보 */}
---
+++ src/pages/s/components/customInfo/TransferInfoTable02.tsx
@@ -0,0 +1,113 @@
+import Icon from "../../../../components/Icon";
+import Table from "../../../../components/table/Table";
+import Tbody from "../../../../components/table/Tbody";
+import Td from "../../../../components/table/Td";
+import Tr from "../../../../components/table/Tr";
+import { icons } from "../../../../constants/icons";
+
+export default function TransferInfoTable02() {
+  const TransferInfoTableData = [
+    {
+      realNumber: "1234567",
+      senderName: "홍길동",
+      senderAlias: "",
+      nameType: "",
+      changeDate: "2024-12-31",
+      bank: "",
+      account: "",
+      amount: "100,000,000",
+      calcType: "",
+      cashType: "",
+      fee: "100,000,000",
+      recipient: "",
+      examiner: "",
+      transferMgmt: "",
+      transferResult: "",
+      status: "등록",
+    },
+  ];
+
+  return (
+    <Table width="min-w-[1751px]">
+      <Tbody>
+        <Tr>
+          <Td type="label" colSpan={2} rowSpan={2}>
+            No.
+          </Td>
+          <Td type="label" rowSpan={2}>
+            실명번호
+          </Td>
+          <Td type="label" rowSpan={2}>
+            송금인 실명
+          </Td>
+          <Td type="label" rowSpan={2}>
+            송금인명
+          </Td>
+          <Td type="label" rowSpan={2}>
+            실명구분
+          </Td>
+          <Td type="label" rowSpan={2}>
+            변동일
+          </Td>
+          <Td type="label" rowSpan={2}>
+            수신은행
+          </Td>
+          <Td type="label" rowSpan={2}>
+            수신계좌
+          </Td>
+          <Td type="label" rowSpan={2}>
+            송금금액
+          </Td>
+          <Td type="label" colSpan={2}>
+            수수료
+          </Td>
+          <Td type="label" rowSpan={2}>
+            수수료
+          </Td>
+          <Td type="label" colSpan={3}>
+            수취조회결과
+          </Td>
+          <Td type="label" rowSpan={2}>
+            타행송금결과
+          </Td>
+          <Td type="label" rowSpan={2}>
+            상태
+          </Td>
+        </Tr>
+        <Tr>
+          <Td type="label">산출구분</Td>
+          <Td type="label">현금구분</Td>
+          <Td type="label">수취인성명</Td>
+          <Td type="label">심사원장</Td>
+          <Td type="label">통합이체관리</Td>
+        </Tr>
+        {TransferInfoTableData.map((item, index) => (
+          <Tr key={index}>
+            <Td type="text">
+              <Icon icon={icons.chk} />
+            </Td>
+            <Td type="text" align="left">
+              {index + 1}
+            </Td>
+            <Td type="text">{item.realNumber}</Td>
+            <Td type="text">{item.senderName}</Td>
+            <Td type="text">{item.senderAlias}</Td>
+            <Td type="text">{item.nameType}</Td>
+            <Td type="text">{item.changeDate}</Td>
+            <Td type="text">{item.bank}</Td>
+            <Td type="text">{item.account}</Td>
+            <Td type="text">{item.amount}</Td>
+            <Td type="text">{item.calcType}</Td>
+            <Td type="text">{item.cashType}</Td>
+            <Td type="text">{item.fee}</Td>
+            <Td type="text">{item.recipient}</Td>
+            <Td type="text">{item.examiner}</Td>
+            <Td type="text">{item.transferMgmt}</Td>
+            <Td type="text">{item.transferResult}</Td>
+            <Td type="text">{item.status}</Td>
+          </Tr>
+        ))}
+      </Tbody>
+    </Table>
+  );
+}
