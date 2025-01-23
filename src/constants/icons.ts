eb08f0ba9fd97e0782199175b4f3e94868534d95
ye1n <uyeinyein@gmail.com>
2025-01-23 17:28:33 +0900

    feat: icons 추가

--- src/constants/icons.ts
+++ src/constants/icons.ts
@@ -51,6 +51,11 @@ import ArrRight from "../assets/icons/arr-right.svg?react";
 import Search from "../assets/icons/search.svg?react";
 import RoundPlus from "../assets/icons/round-plus.svg?react";
 import Chk from "../assets/icons/chk.svg?react";
+import View from "../assets/icons/view.svg?react";
+import Lock from "../assets/icons/lock.svg?react";
+import Unlock from "../assets/icons/unlock.svg?react";
+import Edit from "../assets/icons/edit.svg?react";
+import Remove from "../assets/icons/remove.svg?react";
 
 import TableIcon01 from "../assets/icons/table-icon-01.svg?react";
 import TableIcon02 from "../assets/icons/table-icon-02.svg?react";
@@ -115,6 +120,11 @@ const svgIcons = {
   search: Search,
   roundPlus: RoundPlus,
   chk: Chk,
+  view: View,
+  lock: Lock,
+  unlock: Unlock,
+  edit: Edit,
+  remove: Remove,
 
   tableIcon01: TableIcon01,
   tableIcon02: TableIcon02,
